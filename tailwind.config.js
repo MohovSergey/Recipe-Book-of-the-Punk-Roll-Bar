98492835f09fc10ff40fd34c84f3c65c2543757b
Siarhei <Mohovtsovserg123@gmail.com>
2025-12-06 19:44:42 +0300

    create: registration form has been created

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,15 @@
-const {heroui} = require('@heroui/theme');
 import { heroui } from "@heroui/theme";
-  plugins: [heroui()],
-  content: [
-    "./node_modules/@heroui/theme/dist/components/(form|input|modal|navbar).js"
-],
 
 /** @type {import('tailwindcss').Config} */
-export const content = [
-  "./src/**/*.{js,ts,jsx,tsx}",
-  "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
-];
-
-export const theme = {
-  extend: {},
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  darkMode: "class",
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [heroui()],
 };
-
-export const darkMode = "class"; // управление через класс 'dark'
-export const plugins = [heroui()];
